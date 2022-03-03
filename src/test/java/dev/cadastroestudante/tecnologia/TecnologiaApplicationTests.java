cb034a2640b01fa35d3b1fa7ac89df7592054cd9
Maabia <maabia.ferry16@gmail.com>
2022-03-03 15:00:52 -0300

    inicial

---
+++ src/test/java/dev/cadastroestudante/tecnologia/TecnologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cadastroestudante.tecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnologiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
