ac67a24d9937f7c869f760da2bf8fca7e9017939
marcosxavierdev <sis.marcosxavier@gmail.com>
2022-03-21 08:52:28 -0300

    classes StudentService e StudentServiceImpl criadas

---
+++ src/main/java/dev/cadastroestudante/tecnologia/application/service/StudentServiceImpl.java
@@ -0,0 +1,15 @@
+package dev.cadastroestudante.tecnologia.application.service;
+
+import dev.cadastroestudante.tecnologia.application.dto.StudentResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    @Override
+    public List<StudentResponse> listaEstudante() {
+        return null;
+    }
+}
