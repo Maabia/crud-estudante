b8acbde5f5a8e9ab6b16a958e8da79dacb129803
marcosxavierdev <sis.marcosxavier@gmail.com>
2022-03-21 08:53:03 -0300

    classes StudentRepository e StudentRepositoryImpl criadas

---
+++ src/main/java/dev/cadastroestudante/tecnologia/application/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package dev.cadastroestudante.tecnologia.application.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository {
+}
---
+++ src/main/java/dev/cadastroestudante/tecnologia/infra/StudentRepositoryImpl.java
@@ -0,0 +1,9 @@
+package dev.cadastroestudante.tecnologia.infra;
+
+import dev.cadastroestudante.tecnologia.application.repository.StudentRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepositoryImpl implements StudentRepository {
+
+}
