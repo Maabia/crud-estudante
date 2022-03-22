75f7ac3b3d9207219bffbc275aad3beaac13f90e
marcosxavierdev <sis.marcosxavier@gmail.com>
2022-03-22 09:15:35 -0300

    metodo findAll criado na camada de Repository

--- src/main/java/dev/cadastroestudante/tecnologia/application/repository/StudentRepository.java
+++ src/main/java/dev/cadastroestudante/tecnologia/application/repository/StudentRepository.java
@@ -1,7 +1,11 @@
 package dev.cadastroestudante.tecnologia.application.repository;
 
+import dev.cadastroestudante.tecnologia.domain.Student;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository {
+    List<Student> findAll();
 }
--- src/main/java/dev/cadastroestudante/tecnologia/infra/StudentMongoDBRepository.java
+++ src/main/java/dev/cadastroestudante/tecnologia/infra/StudentMongoDBRepository.java
@@ -4,6 +4,7 @@ import dev.cadastroestudante.tecnologia.domain.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
